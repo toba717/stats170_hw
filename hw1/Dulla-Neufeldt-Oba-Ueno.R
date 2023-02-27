04921e4d725f5ea5052517a546a8b42d8d8774aa
Takao Oba <102635033+toba717@users.noreply.github.com>
2023-02-26 19:02:11 -1000

    Added VAR component

--- hw1/Dulla-Neufeldt-Oba-Ueno.R
+++ hw1/Dulla-Neufeldt-Oba-Ueno.R
@@ -2,7 +2,7 @@
 ### I. Introduction
 library(Quandl)
 library(dygraphs)
-Quandl.api_key("") 
+Quandl.api_key("uSPQnqK5PuPzTydvoB_R") 
 
 retail_electronic = Quandl(code="FRED/RSEASN",type="ts", collapse="monthly", meta=TRUE)
 # Do not include 2020 and 2021 years in our data
@@ -31,9 +31,12 @@ retails_elecronic_test = electronic %>%
 
 
 ### II. Components Features of the dependent variable
-add_decomp_plot <- plot(decompose(retails_electronic_train, type='add')) 
+plot(retails_electronic_train)
+add_decomp_plot <- (decompose(retails_electronic_train, type='add')) 
 mult_decomp_plot <- decompose(retails_electronic_train, type = "mult")
-add_decomp_plot
+plot(add_decomp_plot)
+plot(mult_decomp_plot)
+add_decomp_plot$seasonal
 mult_decomp_plot$seasonal
 
 boxplot_elec <- boxplot(retails_electronic_train ~ cycle(retails_electronic_train))
@@ -106,3 +109,177 @@ total_predict <- predictions * seasonals
 quad_predict <- ts(total_predict)
 
 quad_predict
+
+
+
+electronic = window(retail_electronic, end = c(2019, 12))
+
+plot(electronic)
+elec_log <- log(electronic)
+plot(elec_log)
+
+elec_log_diff  <- diff(elec_log) # normal
+plot(elec_log_diff)
+
+elec_log_diffdiff <- diff(elec_log_diff, lag = 12) # seasonal
+plot(elec_log_diffdiff)
+
+
+par(mfrow = c(1,2))
+acf(elec_log_diffdiff)
+pacf(elec_log_diffdiff)
+
+# generating the model
+model1 <- arima(retails_electronic_train_log, order = c(0,1,1), seas = list(order = c(0,1, 1), 12))
+model1
+
+
+model2 <- arima(retails_electronic_train_log, order = c(0,1,1), seas = list(order = c(0,1, 2), 12))
+model2
+
+
+forecast = predict(model2, n.ahead = 12)
+forecast.value = ts((forecast$pred), start = start(retails_elecronic_test), freq =12)
+
+ts.plot(data.frame(retails_elecronic_test, exp(forecast.value)), col = c("blue", "red"))
+
+
+
+
+############################################################################
+##HW 2
+############################################################################
+
+### Load libraries and API Key
+library(Quandl)
+library(dygraphs)
+Quandl.api_key("uSPQnqK5PuPzTydvoB_R") 
+
+### Load Datasets
+retail_electronic = Quandl(code="FRED/RSEASN",type="ts", collapse="monthly", meta=TRUE)
+# Do not include 2020 and 2021 years in our data
+electronic = window(retail_electronic, end = c(2019, 12))
+# > 200 observations & start: 1992 1
+
+### Examine Data
+retail_electronic # not too long so can view whole dataset
+head(retail_electronic)
+tail(retail_electronic)
+class(retail_electronic)
+str(retail_electronic)
+start(retail_electronic)
+end(retail_electronic)
+frequency(retail_electronic)
+length(retail_electronic)
+
+### Creating a training and testing set
+retails_electronic_train = window(electronic, start = start(electronic), end = c(2019,12), freq = 12)
+retails_electronic_test = window(electronic, start = c(2019, 1), freq = 12)
+
+### Call the raw training dependent variable y
+y = retails_electronic_train
+
+### We look at Time Plot
+plot.ts(y, main = "Retail Sales for Electronics", ylab = "Retail Sales (in Millions of Dollars)")
+### Seasonal Box Plot
+boxplot(y ~ cycle(y), ylab = "Retail Sales (in Millions of Dollars)", xlab = "Month")
+
+
+### To see if the time series has changing variance, get a hint through decomposition
+plot(decompose(y))
+plot(decompose(y, type = "mult"))
+
+
+##### Step 1 is to pre-transform the data
+log.ts = ts(log(y), frequency = 12, start(y))
+sqr.ts = ts(sqrt(y), frequency = 12, start(y))
+qrt.ts = ts((y)^0.25, frequency = 12, start(y))
+
+### Compare data with pre-differenced and pre-transformed data
+par(mfrow = c(2,2))
+plot.ts(y, type = "l", main = "Time Series Plot of Retail Sales for Electronics")
+plot.ts(log.ts, type = "l", main = "Time Series Plot of log(Retail Sales) for Electronics")
+plot.ts(sqr.ts, type = "l", main = "Time Series Plot of sqrt(Retail Sales) for Electronics")
+plot.ts(qrt.ts, type = "l", main = "Time Series Plot of qrt(Retail Sales) for Electronics")
+dev.off()
+
+### Log transformation performs the best, so we will call this y*
+ystar = log.ts
+
+
+# I don't think we need this
+###########################################################################################################
+
+
+
+
+retails_furniture_train = furniture %>% window(end = c(2018, 12))
+retails_retails_train_log = log(retails_furniture_train)
+plot(sqrt(retails_furniture_train))
+plot(log(retails_furniture_train))
+plot((retails_furniture_train))
+
+retails_hobbies_train = hobby %>% window(end = c(2018, 12))
+retails_hobbies_train_log = log(retails_hobbies_train)
+plot(sqrt(retails_hobbies_train))
+plot(log(retails_hobbies_train))
+plot((retails_hobbies_train))
+
+# retails_elecronic_test = electronic %>% window(start = 2019)
+
+
+par(mfrow = c(1,2))
+model1 = lm(retails_electronic_train_log ~ retails_retails_train_log+ retails_hobbies_train_log)
+summary(model1) # find r squared value
+acf(model1$residual, lag = 50)
+pacf(model1$residual, lag = 50) #pacf cuts off, so use AR
+
+resmodel1=arima(residuals(model1), order=c(1,0,0), include.mean=F)
+acf(resmodel1$residuals)
+
+
+resmodel2=arima(residuals(model1), order=c(2,0,0), include.mean=F)
+acf(resmodel2$residuals)
+
+resmodel3=arima(residuals(model1), order=c(12,0,0), include.mean=F)
+acf(resmodel3$residuals)
+
+
+# install.packages("nlme") 
+library(nlme)
+# remove.packages("nlme")
+
+# this takes a long time to run
+models = gls(retails_electronic_train_log ~ retails_retails_train_log+ retails_hobbies_train_log, correlation = corARMA(c(0.3218, 0.0678,  0.0496,  0.0143,  -0.0014,  -0.2160,  0.1706,  0.0265,  0.0315,  -0.0466,  -0.0049,  0.5800), p = 12))
+
+dev.off()
+
+plot(y = residuals(models, type = "normalized"), x = as.vector(time(retails_electronic_train_log)), type = "l")
+abline(h = 0)
+
+acf(ts(residuals(models, type = "normalized")), lag = 50)
+
+
+resmodel3=arima(residuals(model1), order=c(12,0,0), include.mean=F)
+acf(resmodel3$residuals)
+
+
+###################################################
+# Trial 2
+
+# this takes a long time to run
+resmodel4=arima(residuals(model1), order=c(12,0,12), include.mean=F)
+acf(resmodel4$residuals, lag = 50)
+models1 = gls(retails_electronic_train_log ~ retails_retails_train_log+ retails_hobbies_train_log, correlation = corARMA(c(-0.0284,  0.0057,  -0.0253,  -0.0046,  -0.0022,  -0.0285,  -0.0170,  -0.0068,  0.0096,  -0.0131,  0.030,  0.9461, 0.5429,  0.4512,  0.4660,  0.5050,  0.5435,  0.4819,  0.4544,  0.4663,  0.4525,  0.4159,  0.3656,  -0.1197), p = 12, q = 12))
+
+dev.off()
+
+plot(y = residuals(models1, type = "normalized"), x = as.vector(time(retails_electronic_train_log)), type = "l")
+abline(h = 0)
+
+acf(ts(residuals(models1, type = "normalized")), lag = 50)
+
+
+###################################################
+
+###########################################################################################################
